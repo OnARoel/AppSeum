b907b8e7e02f0ae4862833cbf03b6e2e874ff210
hewenyan <hewenyanca@gmail.com>
2022-04-03 13:32:13 -0400

    port 4000

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ dotenv.config();
 
 //if port 4000 is occupied, will go to 5000
 //const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT || 4000
-
+//const PORT = process.env.PORT || 4000
+const PORT = 4000
 //getting the web server. port 4000
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
