87e0d3aeacd0e5deb0e6ccbc34f4fc35be5cff1c
hewenyan <hewenyanca@gmail.com>
2022-04-03 17:36:26 -0400

    proxy test

---
+++ front-end/src/setupProxy.js
@@ -0,0 +1,14 @@
+// Sample of how to setup a proxy middleware with environment variables
+
+//// file: <project_root>/src/setupProxy.js
+import createProxyMiddleware from 'http-proxy-middleware';
+
+module.exports = function(app) {
+  app.use(
+    '/rest',
+    createProxyMiddleware({
+      target: 'http://localhost:'+ process.env.PORT,
+      changeOrigin: true,
+    })
+  );
+};
